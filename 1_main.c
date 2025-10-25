539cce7c08bfe6381b51e8c4efd0d96d5d7ad57f
baizang777 <zx18244358421xx@qq.com>
2025-10-25 15:30:24 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,13 @@
-//
-//
-//
-#include <stdio.h>
+//202510125216
+//zx18244358421xx@qq.com
+//曾瑕
+#include "stdio.h"
 int main()
 {
-    printf("Hello World");
+    float a, b;
+    printf("请输入摄氏温度: ");
+    scanf_s("%f", &a);
+    b = a * ((float)9 / 5) + 32;
+    printf("华氏度为%0.1f\n", b);
     return 0;
 }
